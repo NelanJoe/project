b836c3910ed912f5a6c76524d342c69d1aaca4bc
NelanJoe <nelanjorelat@gmail.com>
2020-09-21 13:11:05 +0700

    project

---
+++ belajarvue/src/components/Navbar.vue
@@ -0,0 +1,69 @@
+<template>
+  <div>
+    <b-navbar toggleable="lg" type="light">
+      <div class="container">
+        <b-navbar-brand href="/">Kulineran</b-navbar-brand>
+
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+        <b-collapse id="nav-collapse" is-nav>
+          <b-navbar-nav>
+            <ul class="navbar-nav mr-auto">
+              <li class="nav-item">
+                <router-link class="nav-link" to="/">Home</router-link>
+              </li>
+              <li class="nav-item">
+                <router-link class="nav-link" to="/foods">Foods</router-link>
+              </li>
+            </ul>
+          </b-navbar-nav>
+
+          <!-- Right aligned nav items -->
+          <b-navbar-nav class="ml-auto">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item">
+                <router-link class="nav-link" to="/keranjang">
+                  Keranjang
+                  <b-icon-bag></b-icon-bag>
+                  <span
+                    class="badge badge-success ml-2"
+                  >{{ updatekeranjang ? updatekeranjang.length : jumlah_pesanans.length }}</span>
+                </router-link>
+              </li>
+            </ul>
+          </b-navbar-nav>
+        </b-collapse>
+      </div>
+    </b-navbar>
+  </div>
+
+  
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "Navbar",
+  data() {
+    return {
+      jumlah_pesanans: [],
+    };
+  },
+  props: ["updatekeranjang"],
+  methods: {
+    setJumlah(data) {
+      this.jumlah_pesanans = data;
+    },
+  },
+  mounted() {
+    axios
+      .get("http://localhost:3000/keranjangs")
+      .then((response) => this.setJumlah(response.data))
+      .catch((error) => console.log(error));
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ belajarvue/src/main.js
@@ -0,0 +1,23 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
+
+import VueToast from 'vue-toast-notification';
+import 'vue-toast-notification/dist/theme-default.css';
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import './assets/css/main.css'
+
+Vue.use(BootstrapVue)
+Vue.use(BootstrapVueIcons)
+Vue.use(VueToast);
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
